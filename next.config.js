11a8e18cc51b23c3c2461a81ef33382ee5652fe1
MuhammadFahadAkbar <fahadakbar200@gmail.com>
2023-07-20 06:35:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose'],
+  },
+  images: {
+    domains: ["occ-0-116-448.1.nflxso.net"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import HeroSection from "./components/HeroSection";
+
+const Page = () => {
+  return (
+    <>
+      <HeroSection title="Let's watch Movie Together" imageUrl="/home.svg" />
+    </>
+  );
+};
+
+export default Page;
